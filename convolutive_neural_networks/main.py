0da32e606d30a4bd95f01e6d400749fbcda08b34
CharlyMercury <ctovarg@upv.edu.mx>
2025-03-17 23:33:31 -0600

    Convolutive neural networks and manage of images

---
+++ convolutive_neural_networks/main.py
@@ -0,0 +1,27 @@
+"""
+    Main file for the project.  
+    
+    This file is used to run the project and test the CNN.
+
+"""
+import sys
+from src.managing_images import managing_images
+from src.convolution_kernels import convolution_kernel
+
+
+def run(program_to_run):
+    """
+        Run the project.
+    """
+    if program_to_run == 'conv_ker':
+        convolution_kernel()
+    if program_to_run == 'mng_imgs':
+        managing_images()
+
+
+if __name__=='__main__':
+    if len(sys.argv) > 1 and sys.argv[1] in ['mng_imgs', 'conv_ker']:
+        run(sys.argv[1])
+    else:
+        print("Invalid command. Please use one of the following commands:")
+        print("mng_imgs, conv_ker")
---
+++ convolutive_neural_networks/src/convolution_kernels.py
@@ -0,0 +1,94 @@
+"""
+
+    This module contains the convolution kernels used in the convolutional layers of the CNN.
+
+    Main python modules used:
+    - numpy (pip install numpy)
+    - opencv-python (pip install opencv-python)
+    - matplotlib (pip install matplotlib)
+
+    Página de ayuda
+    - https://setosa.io/ev/image-kernels/
+
+"""
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def convolution_kernel():
+    """
+        Apply different convolution kernels to an image.
+    """
+
+    # Cargamos la imagen utilizando OpenCV
+    path = r'C:\Users\carlo\OneDrive\Desktop\artificial_vision\convolutive_neural_networks\images\example_1.jpg'
+    img = cv2.imread(path)
+
+    # Convertimos la imagen a RGB (viniendo del formato que lee opencv)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    print(img_rgb.shape)
+
+    # Convertimos la imagen a escalas de gris (para detección de bordes)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    print(img_gray.shape)
+
+    # Configuramos los plots
+    axs_orgl = plt.subplots(1, 2, figsize=(16, 8))[1]
+
+    axs_orgl[0].imshow(img_rgb)
+    axs_orgl[0].set_title("Imagen original")
+    axs_orgl[0].axis('off')
+
+    axs_orgl[1].imshow(img_gray, cmap='gray')
+    axs_orgl[1].set_title("Imagen escala de grises")
+    axs_orgl[1].axis('off')
+
+    plt.show()
+
+    # Diccionario
+    kernels = {
+        "Identidad (imagen original)": np.array([[0, 0, 0],
+                                            [0, 1, 0],
+                                            [0, 0, 0]]),
+        "Blur promedio (para suavizar el ruido)": (1/9)*np.ones((3,3)), 
+        "Blur gausiano (suavizado natural)": (1/16)*np.array([[1, 2, 1],
+                                                            [2, 4, 2],
+                                                            [1, 2, 1]]),
+        "Sobel Horizontal (detecta bordes horizontales)": np.array([[-1, -2, -1],
+                                                                [0, 0, 0],
+                                                                [1, 2, 1]]),
+        "Sobel Vertical (detecta bordes verticales)": np.array([[-1, 0, 1],
+                                                            [-2, 0, 2],
+                                                            [-1, 0, 1]]),
+        "Laplaciano (detecta bordes)": np.array([[0, -1, 0],
+                                            [-1, 4, -1],
+                                            [0, -1, 0]]),
+        "Sharpen (realza detalles)": np.array([[0, -1, 0],
+                                                    [-1, 5, -1],
+                                                    [0, -1, 0]])
+    }
+
+    # Creamos una figura con 2 filas y 4 columnas
+    fig, axs = plt.subplots(2, 4, figsize=(16, 8))
+    axs = axs.flatten()
+
+    # Aplicamos cada kernels del diccionario a la imagen
+    for i, (name, kernel) in enumerate(kernels.items()):
+        if 'Sobel' in name or 'Laplaciano' in name:
+            img_to_use = img_gray
+        else:
+            img_to_use = img_rgb
+
+        convolved = cv2.filter2D(src=img_to_use, ddepth=-1, kernel=kernel)
+
+        cmap_type = 'gray' if len(convolved.shape) == 2 else None
+
+        axs[i].imshow(convolved, cmap=cmap_type)
+        axs[i].set_title(name)
+        axs[i].axis('off')
+
+    # Quitamos los ejes de la última imagen
+    axs[-1].axis('off')
+    plt.tight_layout()
+    plt.show()
