ae3b135de6cf9cd48f9fbb5308a67d9997deeec6
CharlyMercury <ctovarg@upv.edu.mx>
2025-03-19 23:39:56 -0600

    convolution applied to video

--- convolutive_neural_networks/main.py
+++ convolutive_neural_networks/main.py
@@ -6,7 +6,7 @@
 """
 import sys
 from src.managing_images import managing_images
-from src.convolution_kernels import convolution_kernel
+from src.convolution_kernels import convolution_kernel, choose_one_kernel
 
 
 def run(program_to_run):
@@ -15,13 +15,15 @@ def run(program_to_run):
     """
     if program_to_run == 'conv_ker':
         convolution_kernel()
-    if program_to_run == 'mng_imgs':
+    elif program_to_run == 'mng_imgs':
         managing_images()
+    elif program_to_run == 'conv_video':
+        choose_one_kernel()
 
 
 if __name__=='__main__':
-    if len(sys.argv) > 1 and sys.argv[1] in ['mng_imgs', 'conv_ker']:
+    if len(sys.argv) > 1 and sys.argv[1] in ['mng_imgs', 'conv_ker', 'conv_video']:
         run(sys.argv[1])
     else:
         print("Invalid command. Please use one of the following commands:")
-        print("mng_imgs, conv_ker")
+        print("mng_imgs, conv_ker, conv_video")
--- convolutive_neural_networks/src/convolution_kernels.py
+++ convolutive_neural_networks/src/convolution_kernels.py
@@ -17,6 +17,58 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 
+# Diccionario de Kernels de Convolución
+kernels = {
+    "identidad": np.array([
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0]
+    ]),
+
+    "blur": (1 / 9) * np.ones((3, 3)),
+
+    "gaussiano": (1 / 16) * np.array([
+        [1, 2, 1],
+        [2, 4, 2],
+        [1, 2, 1]
+    ]),
+
+    "sobel_x": np.array([
+        [-1, -2, -1],
+        [ 0,  0,  0],
+        [ 1,  2,  1]
+    ]),
+
+    "sobel_y": np.array([
+        [-1,  0,  1],
+        [-2,  0,  2],
+        [-1,  0,  1]
+    ]),
+
+    "laplaciano": np.array([
+        [ 0, -1,  0],
+        [-1,  4, -1],
+        [ 0, -1,  0]
+    ]),
+
+    "sharpen": np.array([
+        [ 0, -1,  0],
+        [-1,  5, -1],
+        [ 0, -1,  0]
+    ])
+}
+
+
+kernel_choose_list = [
+    "identidad", 
+    "blur", 
+    "gaussiano", 
+    "sobel_x", 
+    "sobel_y", 
+    "laplaciano", 
+    "sharpen"]
+
+
 def convolution_kernel():
     """
         Apply different convolution kernels to an image.
@@ -38,7 +90,7 @@ def convolution_kernel():
     print(img_gray.shape)
 
     # Configuramos los plots
-    axs_orgl = plt.subplots(1, 2, figsize=(16, 8))[1]
+    axs_orgl = plt.subplots(1, 2, figsize=(16, 8))
 
     axs_orgl[0].imshow(img_rgb)
     axs_orgl[0].set_title("Imagen original")
@@ -102,3 +154,38 @@ def convolution_kernel():
     axs[-1].axis('off')
     plt.tight_layout()
     plt.show()
+
+
+def convolution_video(kernel):
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+    
+        if not ret:
+            break
+    
+        filtered = cv2.filter2D(frame, -1, kernel)
+        cv2.imshow("Filtered Video", filtered)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def choose_one_kernel():
+
+    kernel_choose = input("Choose one kernel to apply: \n"
+                            "0. identidad (imagen original)\n"
+                            "1. blur (para suavizar el ruido)\n"
+                            "2. gaussiano (suavizado natural)\n"
+                            "3. sobel_x (detecta bordes horizontales)\n"
+                            "4. sobel_y (detecta bordes verticales)\n"
+                            "5. laplaciano (detecta bordes)\n"
+                            "6. sharpen (realza detalles):\n"
+                            "Choose one option: ")
+
+    kernel = kernels.get(kernel_choose_list[int(kernel_choose)])
+    convolution_video(kernel)
+    
